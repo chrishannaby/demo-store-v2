3b1096cef6660af13792a97b9572515518ffbe02
chrishannaby <chrishannaby@gmail.com>
2023-07-25 15:43:10 -0600

    no console.log

--- app/routes/proxy.$.jsx
+++ app/routes/proxy.$.jsx
@@ -4,7 +4,6 @@ export async function loader({request}) {
   const {pathname, search} = new URL(request.url);
   const proxyPath = pathname.replace('/proxy/', '');
   const proxyUrl = shopifyCdn + proxyPath + search;
-  console.log(request.headers);
   const proxyResponse = await fetch(proxyUrl, {
     headers: request.headers,
   });
